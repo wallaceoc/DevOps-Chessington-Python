rej696/chessington-python

Bishop tests and implementation

tests/test_pieces.py
@@ -482,11 +482,11 @@ class TestKnight:
         # Arrange
         board = Board.empty()
         knight = Knight(Player.WHITE)
-        square = Square.at(5, 2)
+        square = Square.at(3, 5)
         board.set_piece(square, knight)
 
         enemy = Pawn(Player.BLACK)
-        enemy_square = Square.at(3, 1)
+        enemy_square = Square.at(1, 4)
         board.set_piece(enemy_square, enemy)
 
         # Act
@@ -501,11 +501,11 @@ class TestKnight:
         # Arrange
         board = Board.empty()
         knight = Knight(Player.WHITE)
-        square = Square.at(5, 2)
+        square = Square.at(3, 5)
         board.set_piece(square, knight)
 
         friendly = Pawn(Player.WHITE)
-        friendly_square = Square.at(3, 1)
+        friendly_square = Square.at(1, 4)
         board.set_piece(friendly_square, friendly)
 
         # Act
@@ -514,3 +514,101 @@ class TestKnight:
         # Assert
         assert friendly_square not in moves
 
+
+class TestBishop:
+
+    @staticmethod
+    def test_bishop_can_move_diagonally():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        expected_moves = [
+            Square.at(0, 2), Square.at(1, 3), Square.at(2, 4), Square.at(4, 6), Square.at(5, 7), 
+            Square.at(1, 7), Square.at(2, 6), Square.at(4, 4), Square.at(5, 3), Square.at(6, 2), Square.at(7, 1)
+        ]
+        assert len(moves) == len(expected_moves)
+        assert sorted(moves) == sorted(expected_moves)
+
+    @staticmethod
+    def test_bishop_can_capture_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        enemy = Pawn(Player.BLACK)
+        enemy_square = Square.at(5, 3)
+        board.set_piece(enemy_square, enemy)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert enemy_square in moves
+
+    @staticmethod
+    def test_bishop_is_blocked_by_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        enemy = Pawn(Player.BLACK)
+        enemy_square = Square.at(5, 3)
+        board.set_piece(enemy_square, enemy)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert Square.at(6, 2) not in moves
+
+    @staticmethod
+    def test_bishop_cannot_capture_friendly_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        friendly = Pawn(Player.WHITE)
+        friendly_square = Square.at(5, 3)
+        board.set_piece(friendly_square, friendly)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert friendly_square not in moves
+
+    @staticmethod
+    def test_bishop_is_blocked_by_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        friendly = Pawn(Player.WHITE)
+        friendly_square = Square.at(5, 3)
+        board.set_piece(friendly_square, friendly)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert Square.at(6, 2) not in moves

wallaceoc/DevOps-Chessington-Python

tests/test_pieces.py
@@ -482,11 +482,11 @@ class TestKnight:
         # Arrange
         board = Board.empty()
         knight = Knight(Player.WHITE)
-        square = Square.at(5, 2)
+        square = Square.at(3, 5)
         board.set_piece(square, knight)
 
         enemy = Pawn(Player.BLACK)
-        enemy_square = Square.at(3, 1)
+        enemy_square = Square.at(1, 4)
         board.set_piece(enemy_square, enemy)
 
         # Act
@@ -501,11 +501,11 @@ class TestKnight:
         # Arrange
         board = Board.empty()
         knight = Knight(Player.WHITE)
-        square = Square.at(5, 2)
+        square = Square.at(3, 5)
         board.set_piece(square, knight)
 
         friendly = Pawn(Player.WHITE)
-        friendly_square = Square.at(3, 1)
+        friendly_square = Square.at(1, 4)
         board.set_piece(friendly_square, friendly)
 
         # Act
@@ -514,3 +514,101 @@ class TestKnight:
         # Assert
         assert friendly_square not in moves
 
+
+class TestBishop:
+
+    @staticmethod
+    def test_bishop_can_move_diagonally():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        expected_moves = [
+            Square.at(0, 2), Square.at(1, 3), Square.at(2, 4), Square.at(4, 6), Square.at(5, 7), 
+            Square.at(1, 7), Square.at(2, 6), Square.at(4, 4), Square.at(5, 3), Square.at(6, 2), Square.at(7, 1)
+        ]
+        assert len(moves) == len(expected_moves)
+        assert set(moves) == set(expected_moves)
+
+    @staticmethod
+    def test_bishop_can_capture_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        enemy = Pawn(Player.BLACK)
+        enemy_square = Square.at(5, 3)
+        board.set_piece(enemy_square, enemy)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert enemy_square in moves
+
+    @staticmethod
+    def test_bishop_is_blocked_by_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        enemy = Pawn(Player.BLACK)
+        enemy_square = Square.at(5, 3)
+        board.set_piece(enemy_square, enemy)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert Square.at(6, 2) not in moves
+
+    @staticmethod
+    def test_bishop_cannot_capture_friendly_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        friendly = Pawn(Player.WHITE)
+        friendly_square = Square.at(5, 3)
+        board.set_piece(friendly_square, friendly)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert friendly_square not in moves
+
+    @staticmethod
+    def test_bishop_is_blocked_by_enemy_pieces():
+
+        # Arrange
+        board = Board.empty()
+        bishop = Bishop(Player.WHITE)
+        square = Square.at(3, 5)
+        board.set_piece(square, bishop)
+
+        friendly = Pawn(Player.WHITE)
+        friendly_square = Square.at(5, 3)
+        board.set_piece(friendly_square, friendly)
+
+        # Act
+        moves = bishop.get_available_moves(board)
+
+        # Assert
+        assert Square.at(6, 2) not in moves